89482dc049c08e77c783874a4f38f8d027138152
Godwin Bruce <godwin.bruce@gmail.com>
2024-03-22 16:39:04 -0700

    config management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
 # installs the package puppet-lint
-package { 'flask':
-  ensure   => '2.1.0',
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
+}
+
+# Using Puppet, install flask
+
+package {'flask':
+ensure   => '2.1.0',
+provider => 'pip3',
 }
