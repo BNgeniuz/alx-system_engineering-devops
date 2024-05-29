96baa7d824959a47407a1d45216bd01933a9497d
Godwin Bruce <godwin.bruce@gmail.com>
2024-05-28 17:10:32 -0700

    top ten update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,10 +7,13 @@ import requests
 def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        for i in range(10):
-            print(data['data']['children'][i]['data']['title'])
-    else:
-        print(None)
+    params = {
+        "limit": 10
+        }
+    response = requests.get(url, headers=headers, params=params,
+            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
