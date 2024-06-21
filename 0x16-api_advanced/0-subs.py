698300dc27709d53b3ac49efbc9b7ff271f79f05
Godwin Bruce <godwin.bruce@gmail.com>
2024-06-20 17:15:59 -0700

    subs count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,11 +13,9 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{subreddit}/about.json".format(subreddit)
     headers = {
             'User-Agent': "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-            }
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 404:
+    if response.status_code == 200:
         return 0
-
-    data = response.json().get("data")
-    return = data.get("subscribers")
+    results = response.json().get("data")
+    return = results.get("subscribers")
